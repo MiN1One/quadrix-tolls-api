32ad2671b7a99e58907ce74de2cda6242c5c7da8
Asilbek <asilbekturgunboevv@gmail.com>
2026-01-09 16:21:12 +0000

    Remove cors

--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,34 @@ import { NestFactory } from '@nestjs/core';
 import { appConfig } from './app.config';
 import { AppModule } from './app.module';
 
-const allowedOrigins = ['https://app.quadrix.ai', 'http://localhost:5173'];
+const allowedOrigins = [
+  'https://app.quadrix.ai',
+  'http://localhost:5173',
+];
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    cors: true,
+    cors: {
+      origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+
+        if (allowedOrigins.includes(origin)) {
+          return callback(null, true);
+        }
+
+        return callback(new Error('Not allowed by CORS'), false);
+      },
+      credentials: true,
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    },
   });
+
   app.useGlobalPipes(new ValidationPipe());
+
   const config = app.get<ConfigType<typeof appConfig>>(appConfig.KEY);
   await app.listen(config.port);
 
-  app.enableCors({
-    origin: allowedOrigins,
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  });
-
   Logger.log(`Listening on port ${config.port}`);
 }
+
 bootstrap();
